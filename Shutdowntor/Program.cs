47396f594d927b8eb7440ca26f171ad89dc2c014
stevetsang852 <72596467+stevetsang852@users.noreply.github.com>
2026-09-24 17:09:05 +0800

    Use StartupOptions for CLI parsing while keeping ArgsReader for the form.

--- Shutdowntor/Program.cs
+++ Shutdowntor/Program.cs
@@ -1,22 +1,12 @@
-﻿using Shutdowntor.Command;
 using Shutdowntor.Common;
 using System;
-using System.Collections.Generic;
-
-using System.IO;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Shutdowntor
 {
     static class Program
-    {    
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+    {
         [STAThread]
         static void Main(string[] args)
         {
@@ -26,34 +16,21 @@ namespace Shutdowntor
                 {
                     if (!mutex.WaitOne(0, false))
                     {
-                        //MessageBox.Show("Program ONLY can open one", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         UIControl.SingleApplicationController.Instance.ShowRunningApp();
                         return;
                     }
+
                     ArgsReader.Instance.RegisterArg<bool>(ArgsReader.ArgOption.debug);
                     ArgsReader.Instance.RegisterArg<string>(ArgsReader.ArgOption.datetime);
                     ArgsReader.Instance.RegisterArg<bool>(ArgsReader.ArgOption.hide);
                     ArgsReader.Instance.RegisterArg<string>(ArgsReader.ArgOption.auto);
                     ArgsReader.Instance.Read(args);
 
-                    string argDatetime = ArgsReader.Instance.GetArg<string>(ArgsReader.ArgOption.datetime);
-
-                    //bool autoStart = ArgsReader.Instance.CheckArg(ArgsReader.ArgOption.auto);
-                    bool autoStart = ArgsReader.Instance.GetArg<bool>(ArgsReader.ArgOption.auto);
-                    //bool autoSetDateTime = ArgsReader.Instance.CheckArg(ArgsReader.ArgOption.datetime);
-                    bool autoSetDateTime = ArgsReader.Instance.GetArg<bool>(ArgsReader.ArgOption.datetime);
-
-                    string action = ArgsReader.Instance.GetArg<string>(ArgsReader.ArgOption.auto);
-                    DateTime targetDateTime = DateTime.Now.AddDays(1);
-                    
-                    if (autoSetDateTime && argDatetime.Length == Global.DateTimeFormat.Length)
-                    {
-                        targetDateTime = DateTime.ParseExact(argDatetime, Global.DateTimeFormat, null);
-                    }
+                    var options = StartupOptions.Parse(args);
 
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
-                    Application.Run(new Main(autoStart, action, targetDateTime, autoSetDateTime));
+                    Application.Run(new Main(options.AutoStart, options.ActionToken, options.TargetDateTime, options.HasDateTime));
                 }
             }
             catch (Exception ex)
